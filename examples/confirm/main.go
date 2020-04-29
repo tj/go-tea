8a02450a52ac7f0a3ca919ac7f9228f13c75e8ec
TJ Holowaychuk <tj@vision-media.ca>
2020-04-29 10:08:06 +0100

    add confirmation example

---
+++ examples/confirm/main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/tj/go-tea"
+	"github.com/tj/go-tea/input"
+	"github.com/tj/go-terminput"
+)
+
+// Model struct.
+type Model struct {
+	ProjectID string
+	Confirm   input.Model
+	Confirmed bool
+}
+
+// initialize function.
+func initialize(ctx context.Context) (tea.Model, tea.Cmd) {
+	return Model{
+		ProjectID: "my-project",
+	}, nil
+}
+
+// update function.
+func update(ctx context.Context, msg tea.Msg, model tea.Model) (tea.Model, tea.Cmd) {
+	m := model.(Model)
+	m.Confirm = input.Update(msg, m.Confirm)
+
+	switch msg := msg.(type) {
+	case *terminput.KeyboardInput:
+		switch msg.Key() {
+		case terminput.KeyEnter:
+			if m.Confirm.Value == m.ProjectID {
+				m.Confirmed = true
+				return m, tea.Quit
+			}
+			bell()
+			return m, nil
+		case terminput.KeyEscape:
+			return m, tea.Quit
+		case terminput.KeyRune:
+			switch r := msg.Rune(); r {
+			case 'q':
+				return m, tea.Quit
+			}
+		}
+	}
+
+	return m, nil
+}
+
+// view function.
+func view(ctx context.Context, model tea.Model) string {
+	w := new(bytes.Buffer)
+	m := model.(Model)
+
+	// padding
+	fmt.Fprintf(w, "\n")
+	defer fmt.Fprintf(w, "\n")
+
+	// confirm
+	if m.Confirmed {
+		fmt.Fprintf(w, "  Deleted %q.", m.ProjectID)
+	} else {
+		fmt.Fprintf(w, "  Enter %q to confirm deletion: %s", m.ProjectID, input.View(m.Confirm))
+	}
+
+	return w.String()
+}
+
+// bell sound.
+func bell() {
+	fmt.Printf("\a")
+}
+
+func main() {
+	program := tea.NewProgram(initialize, update, view)
+	err := program.Start(context.Background())
+	if err != nil {
+		log.Fatalf("error: %s\n", err)
+	}
+}
